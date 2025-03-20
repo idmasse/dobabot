0b6be001f4c349cd1d89194cd6cc238cbe19adce
idmasse <idmasse@icloud.com>
2025-03-20 14:52:03 -0700

    updates selenium setup

--- utils/selenium_setup.py
+++ utils/selenium_setup.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 def get_driver():
     logger.info("Initializing Chrome driver...")
     chrome_options = Options()
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-dev-shm-usage')
